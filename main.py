8159f554d5c0182030cce8b346aa59f2e80fd9d8
anbreaker <antunez19@gmail.com>
2020-01-21 23:22:39 +0100

    Pantalla principal con fondo pantalla

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
-from pygame import *
+import pygame as pg
 from pygame.locals import *
 import sys
 from random import randint
-from entities import *
 
-init()
+FPS = 60
 
 
 class Game:
-    clock = time.Clock()
+    clock = pg.time.Clock()
 
     def __init__(self):
-        self.screen = display.set_mode((800, 600))
-        display.set_caption('Hola mundo!')
+        #Tamaño pantalla
+        self.screen = pg.display.set_mode((800, 600))
+        pg.display.set_caption('Arkanoid pyGame!')
 
-        self.background_color = (150, 150, 222)
+        self.background_img = pg.image.load('resources/fondo.png').convert()
+        
+    def gameOver(self):
+        pg.quit()
+        sys.exit()
+        
+    def handleEvents(self):
+        for event in pg.event.get():
+            if event.type == QUIT:
+                self.gameOver()
+    
+    def mainloop(self):
+        while True:
+            dt = self.clock.tick(FPS)
+            
+            self.handleEvents()
+            self.screen.blit(self.background_img, (0,0))
+            pg.display.flip()
+            
+    
+if __name__ == '__main__':
+    pg.init()
+    game = Game()
+    game.mainloop()
